615138edfbaef1c052cf0e4a64a76ef06797a30b
Eduardo <eduardo@dudstecnologia.com.br>
2019-11-17 03:06:35 -0400

    Lista finalizada

--- src/pages/Contatos.vue
+++ src/pages/Contatos.vue
@@ -1,6 +1,18 @@
 <template>
   <q-page padding>
-    <p>Lista de Contatos : {{ name }}</p>
+
+    <q-list bordered separator>
+
+      <q-item clickable v-ripple v-for="c in contatos" :key="c.id" @click="selecionaContato(c.id)">
+        <q-item-section>
+          <q-item-label>{{ c.nome }}</q-item-label>
+          <q-item-label caption>{{ c.email }}</q-item-label>
+          <q-item-label caption>{{ c.telefone }}</q-item-label>
+        </q-item-section>
+      </q-item>
+
+    </q-list>
+
   </q-page>
 </template>
 
@@ -8,18 +20,25 @@
 export default {
   data () {
     return {
-      name: '',
+      contatos: []
     }
   },
+  created () {
+    this.getContatos();
+  },
   methods: {
-    user () {
-      this.$axios.get('/user', this.user)
+    getContatos () {
+      this.$axios.get('/contatos', this.user)
         .then((res) => {
-          console.log(res);
+          // console.log(res);
+          this.contatos = res.data;
         })
         .catch((err) => {
           console.log(err);
         });
+    },
+    selecionaContato(id) {
+      console.log('ID: ' + id);
     }
   }
 }
